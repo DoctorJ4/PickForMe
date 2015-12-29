4e83a8eb0dbec99a7ff8461f6f99e9d5fbfd04b2
Jesse <jrdodson@pluto.dsu.edu>
2015-12-29 11:43:59 -0600

    Can now name lists

--- app/src/main/java/com/example/ray/pickforme/ListViewActivity.java
+++ app/src/main/java/com/example/ray/pickforme/ListViewActivity.java
@@ -1,16 +1,15 @@
 package com.example.ray.pickforme;
 
 import android.app.Activity;
-import android.graphics.Color;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
-import android.view.ContextThemeWrapper;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.util.ArrayList;
@@ -22,6 +21,7 @@ public class ListViewActivity extends Activity {
     DBHelper dbHelper;
     PickList list = new PickList();
     LinearLayout listLayout;
+    Context thisContext = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -108,18 +108,45 @@ public class ListViewActivity extends Activity {
             Toast.makeText(this, "You need to supply me with more than one thing to pick from.", Toast.LENGTH_SHORT).show();
         }
         else {
-            List<String> saveList = new ArrayList<>();
-            EditText picked;
-            String temp;
-            for (int i = 0; i < editBoxIdList.size(); i++) {
-                picked = (EditText) findViewById(editBoxIdList.get(i));
-                if (picked.getText().length() > 0) {
-                    saveList.add(picked.getText().toString());
-                }
-            }
-            dbHelper.addList("New List!!!", saveList);
-            Toast.makeText(this, "List Saved", Toast.LENGTH_SHORT).show();
+            getNameAndAdd();
         }
+    }
+
+    private void getNameAndAdd(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Title");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String getNewName = input.getText().toString();
+                List<String> saveList = new ArrayList<>();
+                EditText picked;
+                for (int i = 0; i < editBoxIdList.size(); i++) {
+                    picked = (EditText) findViewById(editBoxIdList.get(i));
+                    if (picked.getText().length() > 0) {
+                        saveList.add(picked.getText().toString());
+                    }
+                }
+                dbHelper.addList(getNewName, saveList);
+                Toast.makeText(thisContext, "List Saved", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
     }
 
 }
